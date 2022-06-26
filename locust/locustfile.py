1c77f9c64a7624128063867db3c0b57aff528415
adrilleros2 <adrilleros2>
2022-06-26 13:29:38 +0200

    add extension demo

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,7 +1,20 @@
-from locust import HttpUser, TaskSet, task, between
+from locust import HttpUser, TaskSet, task, between, events
 import random
 
 class APICalls(TaskSet):    
+
+    @events.init_command_line_parser.add_listener
+    def _(parser):
+        parser.add_argument("--my-argument", type=str, default="", help="It's working")
+
+
+    @events.test_start.add_listener
+    def _(environment, **kw):
+        print(f"Custom argument supplied: {environment.parsed_options.my_argument}")
+
+
+
+
     @task()
     def getpost(self):
         post_id = random.randint(1, 100)
